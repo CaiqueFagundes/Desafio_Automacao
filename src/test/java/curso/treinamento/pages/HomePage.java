53aee20b66f973d7052cce87940edd3222492df7
Caique Fagundes Bispo <kaiq.sg1@gmail.com>
2019-05-25 00:24:14 -0300

    Ajustes nos roteiros de teste e construcao da pageobjets

--- src/test/java/curso/treinamento/pages/HomePage.java
+++ src/test/java/curso/treinamento/pages/HomePage.java
@@ -15,6 +15,69 @@ public class HomePage {
 	
 	
 	
+	//Opções do Menu 
+	
+	@FindBy (xpath="//*[@href='https://www.phptravels.net/admin-portal/admin']/i[@class='fa fa-desktop']")
+    WebElement  Dashboard;
+	
+	@FindBy (xpath=" //*[@href=\"https://www.phptravels.net/admin-portal/admin/updates/\"]")
+    WebElement Update;
+	
+	@FindBy (xpath=" //*[@class='fa fa-cube']")
+    WebElement Modules;
+	
+	@FindBy (xpath="//a[@class='collapsed']")
+    WebElement General;
+	
+	@FindBy (xpath="//a [@href='#ACCOUNTS']")
+    WebElement  Accounts;
+	
+	@FindBy ( xpath = "//ul[@class= 'collapse wow fadeIn animated list-unstyled'] [@id='CMS']")
+	WebElement CMS;
+	
+	@FindBy (xpath="//i[@class= 'w30 fa fa-building-o']")
+    WebElement Hotels;
+	
+	@FindBy (xpath="//i[@class= 'w30 fa fa-plane']")
+    WebElement Flights;
+	
+	@FindBy (xpath="//ul[@id = 'Tours']")
+    WebElement Tours;
+	
+	@FindBy (xpath="//i [@class = 'w30 fa fa-car']")
+    WebElement Cars;
+	
+	@FindBy (xpath="//ul[@id='Blog']")
+    WebElement Blog;
+	
+	@FindBy (xpath="//ul[@id='Locations']")
+    WebElement Locations;
+	
+	@FindBy (xpath="//ul[@id='SPECIAL_OFFERS']")
+    WebElement SPECIALOFFERS;
+	
+	@FindBy (xpath="//i [@class = 'fa fa-asterisk']")
+    WebElement Coupons;
+	
+	@FindBy (xpath="//i [@class = 'fa fa-envelope']")
+    WebElement Newsletter;
+	
+	@FindBy (xpath="//i [@class = 'fa fa-list w30']")
+    WebElement Bookings;
+	
+	@FindBy (xpath="// a [@class = 'article']")
+    WebElement Documentation;
+	
+		
+	
+	
+	// Metodos 
+	
+	public void subMenuGeneral() {
+		
+		General.click();
+	}
+	
 	public boolean checkAposLogin() {
 		return texto.isDisplayed();
 		
--- src/test/resources/features/itens_menu.feature
+++ src/test/resources/features/itens_menu.feature
@@ -4,17 +4,17 @@ Funcionalidade: Itens do Menu
 
 Contexto:
 	Dado que eu esteja na tela de login
-    Quando faço login com o usuário "admin" e senha "admin"
+    Quando faço login com o usuário "admin@phptravels.com" e senha "demoadmin"
     Então sou autenticado com sucesso
     
-    @Sprint3 @MenuCatalog
-    Cenário: Itens apresentados no menu Catalog
+    @Sprint3 @SubMenuAccounts
+    Cenário: Itens apresentados no menu Principal
     	Dado que eu esteja na tela Home
-    	Quando acesso o menu Catalog
-    	Então são apresentados os itens "Catalogs", "Categories", "Base Product", "All Products", "Product Bundles", "Add-on Products", "Product Groups", "Product Options", "Cart Rules", "Manage Inventory" e "Locations"
+    	Quando acesso o menu Accounts
+    	Então são apresentados os itens "Admins", "Suppliers", "Customers", "GuestCustomers"
 
     @Sprint4 @MenuPricing
      Cenário: Itens apresentados no menu Pricing
      Dado que eu esteja na tela Home
      Quando acesso o menu Pricing
-     Então sao apresentados os itens do menu "Offers", "Price Lists", "Contracts", "Sales" e "Offer Codes" 
+     Então sao apresentados os itens do menu "  Dashboard", "Updates", " Modules", " General", " Accounts                ", "CMS", "Hotels" e "Flights"  
